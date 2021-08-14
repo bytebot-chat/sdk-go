fae607fe9a79d0f399e92058e4dc0aa5d4ccfb28
Bren Briggs <code@fraq.io>
2021-08-14 11:12:32 -0400

    Add Connect method

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module bytebot-chat/sdk-go
 
 go 1.14
+
+require (
+	github.com/go-redis/redis/v8 v8.11.3
+	github.com/rs/zerolog v1.23.0
+)
--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package bytebot
 
-func main() {}
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/rs/zerolog/log"
+)
+
+func Connect(addr string) (*redis.Client, error) {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Ping(ctx).Err()
+	if err != nil {
+		time.Sleep(3 * time.Second)
+		err := rdb.Ping(ctx).Err()
+		if err != nil {
+			log.Error().Err(err)
+		}
+	}
+
+	return rdb, err
+}
